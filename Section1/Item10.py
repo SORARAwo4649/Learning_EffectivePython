60d2758f0d7b753bc5a3038d502fb077982d2004
SORARAwo4649 <k11274649@icloud.com>
2020-10-10 15:23:44 +0900

    Understanded a little about scopegit add Item10.py

--- Section1/Item10.py
+++ Section1/Item10.py
@@ -13,7 +13,7 @@ fresh_fruit = {
 
 # レモネードを作るのにレモンを使う
 def make_lemonade(count):
-    fresh_fruit['lemon'] -= 1
+    fresh_fruit['lemon'] = count - 1
     if fresh_fruit['lemon'] <= 0:
         fresh_fruit.pop('lemon')
     print('レモネード完成！！')
@@ -21,17 +21,24 @@ def make_lemonade(count):
 
 # サイダーを作るのにはアップルが4個必要らしい
 def make_cider(count):
-    fresh_fruit['apple'] -= 4
+    fresh_fruit['apple'] = count - 4
     if fresh_fruit['apple'] <= 0:
         fresh_fruit.pop('apple')
     print('サイダー完成！！')
+
+
+# バナナスムージーを作るのにバナナが2本必要
+# スライスバナナを作る
+def slice_bananas(count):
+    fresh_fruit['banana'] -= 2
+    fresh_fruit['slice_banana'] += 1
 
 
 def out_of_stock():
     print('在庫切れ')
 
 
-# getの第2引数は、第一引数のキーが存在しなかったときの返り値
+# getの第2引数は第一引数のキーが存在しなかったときの返り値
 # レモネードの注文
 if count := fresh_fruit.get('lemon', 0):
     make_lemonade(count)
@@ -39,5 +46,15 @@ else:
     out_of_stock()
 
 # サイダーの注文
-if (count := fresh_fruit.get('apple')) >= 4:
+if (count := fresh_fruit.get('apple', 0)) >= 4:
     make_cider(count)
+
+# バナナスムージーの注文
+# バナナの在庫確認
+if (count := fresh_fruit.get('banana', 0)) >= 2:
+    # スライスバナナの在庫確認
+    if not fresh_fruit['slice_banana']:
+        # スライスバナナの容器の準備
+        fresh_fruit |= {'slice_banana': 0}
+    # スライスバナナの作成
+    slice_bananas(count)
